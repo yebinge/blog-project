0444ea1def1bc68336e8fa8f4e9cd85675ad396f
yebinge <yebing@student.unimelb.edu.au>
2022-02-22 13:43:24 +0800

    Update templates

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
         return self.comments.filter(approved_comment=True)
 
     def get_absolute_url(self):
-        return reverse('post_detail', kwargs={'pk': self.ok})
+        return reverse('post_detail', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.title
